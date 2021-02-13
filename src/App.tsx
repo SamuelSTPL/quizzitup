2cd86e27379fc45a0d9cbfbbab3e2e330e025c13
SamuelSTPL <samuelstpierrelariviere@gmail.com>
2021-02-12 22:54:22 -0500

    Created app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import QuestionsCard from "./components/QuestionsCard";
+import { fetchQuizzQuestions } from "./Api";
 
-function App() {
-  return <div>Quizz</div>;
-}
+import BackgroundVideo from "./components/BeachBackground";
+import ChooseBackground from "./components/ChooseBackground";
+
+// types
+import { QuestionState, Difficulty } from "./Api";
+
+// styles
+import { GlobalStyle, Wrapper } from "./app.styles";
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAsnwer] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+  const [background, setBackground] = useState("beach");
+
+  console.log(questions);
+
+  const startTrivia = async () => {
+    // TODO Add error handling//////////////////////////////////////////////
+    setLoading(true);
+    setGameOver(false);
+
+    // Fetch new questions from the API
+    const newQuestions = await fetchQuizzQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+    setQuestions(newQuestions);
+    setScore(0);
+    setNumber(0);
+    setUserAsnwer([]);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      const answer = e.currentTarget.value;
+      const correct = questions[number].correct_answer === answer;
+      if (correct) setScore((prev) => prev + 1);
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAsnwer((prev) => [...prev, answerObject]);
+    }
+  };
+
+  const nextQuestion = () => {
+    const nextQuestion = number + 1;
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
+
+  return (
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <h1>Quizz It Up</h1>
+        {gameOver || userAnswer.length === TOTAL_QUESTIONS ? (
+          <button className="start" onClick={() => startTrivia()}>
+            Let's Go!
+          </button>
+        ) : null}
+        {!gameOver ? <p className="score">Score:{score}</p> : null}
+        {loading && <p>Loading...</p>}
+        {!loading && !gameOver && (
+          <QuestionsCard
+            questionNbr={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswer ? userAnswer[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {!gameOver &&
+        !loading &&
+        userAnswer.length === number + 1 &&
+        number !== TOTAL_QUESTIONS - 1 ? (
+          <button className="next" onClick={() => nextQuestion()}>
+            Next
+          </button>
+        ) : null}
+        <ChooseBackground setBackground={setBackground} />
+        <BackgroundVideo background={background} />
+      </Wrapper>
+    </>
+  );
+};
 
 export default App;
